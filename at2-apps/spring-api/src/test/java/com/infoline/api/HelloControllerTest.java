58b63cf56ef6dbbb9efea35700acd0604d1473bd
root <root@debian1.myguest.virtualbox.org>
2026-01-26 15:12:30 +0100

    AT2: dockerized Spring API + k8s manifests (clean repo)

---
+++ at2-apps/spring-api/src/test/java/com/infoline/api/HelloControllerTest.java
@@ -0,0 +1,11 @@
+package com.infoline.api;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloControllerTest {
+  @Test
+  void basicTest() {
+    assertTrue(true);
+  }
+}
